4213679456b70942f9ae096b2d962c9044df1434
squallmar <marcelmendes05@gmail.com>
2025-09-11 12:32:36 -0300

    feat: Fix final da conexao com o banco de dados

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,18 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+// Carrega as variáveis de ambiente apenas localmente
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
 
 const connectionString = process.env.DATABASE_URL || 'postgres://usuario:mM202038@localhost:5432/bolao_db';
 
 const pool = new Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
